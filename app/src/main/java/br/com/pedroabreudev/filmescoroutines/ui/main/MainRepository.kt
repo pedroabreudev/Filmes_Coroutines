fec3d46f5328cd6bb813de1845cb8f1da6b2e932
Pedro Henrique <pedroabreu.dev@gmail.com>
2021-11-04 15:45:43 -0400

    Projeto Concluido

---
+++ app/src/main/java/br/com/pedroabreudev/filmescoroutines/ui/main/MainRepository.kt
@@ -0,0 +1,30 @@
+package br.com.pedroabreudev.filmescoroutines.ui.main
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
+
+class MainRepository {
+
+    fun getFilmes(callback: (filmes: List<Filme>) -> Unit) {
+        Thread(Runnable {
+            Thread.sleep(3000)
+            callback.invoke(
+                listOf(
+                    Filme(1, "Título 01"),
+                    Filme(2, "Título 02"),
+                )
+            )
+        }).start()
+    }
+
+    suspend fun getFilmesCoroutines(): List<Filme> {
+        return withContext(Dispatchers.Default) {
+            delay(3000)
+            listOf(
+                Filme(1, "Título 01"),
+                Filme(2, "Título 02"),
+            )
+        }
+    }
+}
